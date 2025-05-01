f5fd3502579cd66b50c80a31c328a38e5d32728d
ArataUji <ujileather@gmail.com>
2025-05-01 19:20:18 +0900

    first commit

---
+++ backend/webapi/src/test/kotlin/smarthome/webapi/webapi/WebapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package smarthome.webapi.webapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
