faf03a280c155769596006a25995a239c391d245
ArataUji <sep29h12@gmail.com>
2025-06-01 23:05:41 +0900

    MQTTブローカ用のインスタンスとその関連リソースの追加

--- terraform/modules/secret-manager/secret-manager.tf
+++ terraform/modules/secret-manager/secret-manager.tf
@@ -14,25 +14,18 @@ resource "google_secret_manager_secret" "mqtt_username" {
   }
 }
 
-resource "google_secret_manager_secret" "mqtt_password" {
+resource "google_secret_manager_secret" "mqtt_web_password" {
   project   = var.project
-  secret_id = "MQTT_PASSWORD"
+  secret_id = "MQTT_WEB_PASSWORD"
   replication {
     auto {}
   }
 }
 
-resource "google_secret_manager_secret_version" "mqtt_broker_url_version" {
-  secret      = google_secret_manager_secret.mqtt_broker_url.id
-  secret_data = var.mqtt_broker_url
-}
-
-resource "google_secret_manager_secret_version" "mqtt_username_version" {
-  secret      = google_secret_manager_secret.mqtt_username.id
-  secret_data = var.mqtt_username
-}
-
-resource "google_secret_manager_secret_version" "mqtt_password_version" {
-  secret      = google_secret_manager_secret.mqtt_password.id
-  secret_data = var.mqtt_password
+resource "google_secret_manager_secret" "mqtt_ctrl_password" {
+  project   = var.project
+  secret_id = "MQTT_CTRL_PASSWORD"
+  replication {
+    auto {}
+  }
 }
--- terraform/modules/secret-manager/variables.tf
+++ terraform/modules/secret-manager/variables.tf
@@ -2,18 +2,3 @@ variable "project" {
   description = "GCPプロジェクトID"
   type        = string
 }
-
-variable "mqtt_broker_url" {
-  description = "MQTTブローカーのURL"
-  type        = string
-}
-
-variable "mqtt_username" {
-  description = "MQTTブローカーのユーザー名"
-  type        = string
-}
-
-variable "mqtt_password" {
-  description = "MQTTブローカーのパスワード"
-  type        = string
-}
